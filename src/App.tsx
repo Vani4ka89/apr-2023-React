08d93e5d012510a35a6d78dace1ef1a3eb3f14bb
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-04-24 19:39:08 +0300

    hw3

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default App;
+
+
+// Зробити компонент, в якому буде форма, за допомоги якої можливо створити нового юзера постовим запитом
+// на http://jsonplaceholder.typicode.com/users
+//     Зробити компонент, в якому буде форма, за допомоги якої можливо створити новий комментар постовим запитом
+//     на http://jsonplaceholder.typicode.com/comments
